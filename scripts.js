80e7ec65680891c273c0e2f5bfcb38485b369a58
CaioDetore <caio.detore@sou.unifeob.edu.br>
2024-11-27 19:31:59 -0300

    feat: adicionar item na lista

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
-// Seleciona os elementos do formulário
+// Seleciona os elementos do formulário.
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const expense = document.getElementById("expense");
 const category = document.getElementById("category");
 
-// Formatar valor do input
+// Elementos da lista.
+const expenseList = document.querySelector("ul");
+
+// Formatar valor do input.
 amount.oninput = () => {
   // "/\D/g" regex que valida somente digitos
   let value = amount.value.replace(/\D/g, "");
@@ -33,7 +36,42 @@ form.onsubmit = (event) => {
     category_name: category.options[category.selectedIndex].text,
     amount: amount.value,
     created_at: new Date(),
-  }
+  };
 
-  console.log(newExpense)
+  expenseAdd(newExpense);
+};
+
+function expenseAdd(newExpense) {
+  try {
+    // criando elemento.
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("expense");
+
+    const expenseIcon = document.createElement("img");
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
+    expenseIcon.setAttribute("alt", newExpense.category_name);
+
+    // cria info da despesa.
+    const expenseInfo = document.createElement("div");
+    expenseInfo.classList.add("expense-info");
+
+    const expenseName = document.createElement("strong");
+    expenseName.textContent = newExpense.expense;
+
+    const expenseCategory = document.createElement("span");
+    expenseCategory.textContent = newExpense.category_name;
+
+    // adicionando nome e categoria na info.
+    expenseInfo.append(expenseName, expenseCategory);
+
+    // Adicionando informações no item.
+    expenseItem.append(expenseIcon, expenseInfo);
+
+    // Adicionando informações na lista.
+    expenseList.append(expenseItem);
+
+  } catch (error) {
+    alert("Não foi possivel atualizar a lista de despesas.");
+    console.error(error);
+  }
 }
